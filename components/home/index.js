c0994754061f89b2680b99d433f7d7db802eda85
Bin <bin@cacdi.com>
2022-08-23 17:46:47 +0800

    feat: ar.js

--- components/home/index.js
+++ components/home/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import Nav from "components/Nav";
-import ViewpointGuidPage from "components/Nav/ViewpointGuide/ViewpointGuidPage";
-import ViewpointerPage from "components/ViewpointerPage";
-import ImageExamplePage from "components/ImageExamplePage";
-import Title from "components/Title";
-import InstructionPage from "components/InstructionPage";
-import Entry from "components/Entry";
+// import Nav from "components/Nav";
+// import ViewpointGuidPage from "components/Nav/ViewpointGuide/ViewpointGuidPage";
+// import ViewpointerPage from "components/ViewpointerPage";
+// import ImageExamplePage from "components/ImageExamplePage";
+// import Title from "components/Title";
+// import InstructionPage from "components/InstructionPage";
+// import Entry from "components/Entry";
 import styles from "./index.module.scss";
 const Home = () => {
   return (
@@ -16,7 +16,7 @@ const Home = () => {
       {/* <ViewpointerPage /> */}
       {/* <InstructionPage /> */}
       {/* <Title /> */}
-      <Nav />
+      {/* <Nav /> */}
     </div>
   );
 };
